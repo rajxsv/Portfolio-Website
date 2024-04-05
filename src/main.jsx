81657bebeab6fb25e3967e8c5b5f5647186fbb39
Rajpreet Singh <rajpreetsingh@192.168.1.72>
2024-04-05 23:13:44 +0530

    Added SidePanel

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Blogs from "./pages/Blogs.jsx";
+import Projects from "./pages/Projects.jsx";
+import InterviewExperience from "./pages/InterviewExperience.jsx";
+import Main from "./pages/Main.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Main />
+      },
+      {
+        path: "/blogs",
+        element: <Blogs />,
+      },
+      {
+        path: "/projects",
+        element: <Projects />,
+      },
+      {
+        path: "/interview-experiences",
+        element: <InterviewExperience />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
---
+++ src/pages/Blogs.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Blogs() {
+  return (
+    <div className="w-full flex flex-col gap-3 items-center text-white bg-gray-900 max-w-4xl p-5">
+      <header className="text-center mb-5 bg-gray-800 p-3 rounded-md max-w-2xl">
+        <h1 className="text-2xl ">Blogs</h1>
+      </header>
+    </div>
+  );
+}
